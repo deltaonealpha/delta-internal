70027518397194e45cc7bf14bcd1439f8cf793f5
Pranav Balaji <balaji.pranav@outlook.in>
2021-03-15 18:43:38 +0530

    Zephyrus migration

---
+++ degenswrite-backend/simple-django-login/mysite/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+
+from mysite.core import views as core_views
+
+
+urlpatterns = [
+    url(r'^$', core_views.home, name='home'),
+    url(r'^login/$',auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    url( r'^logout/$',auth_views.signup, name='signup'),
+]
